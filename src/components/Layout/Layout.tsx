f3eeadee6a68c6f9d15bc7d1d9153dbf101e8f60
박나리 <>
2021-08-25 01:35:20 +0900

    fix: GNB UI 수정 & mainSection 외 다른 부분에도 background 적용 #24

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,16 +6,20 @@ interface ILayoutProps {
 }
 const Layout: React.FC<ILayoutProps> = ({ children, setTheme }) => {
   return (
-    <>
+    <StyledLayoutContainer>
       <GNB setTheme={setTheme} />
       <StyledMain>{children}</StyledMain>
-    </>
+    </StyledLayoutContainer>
   );
 };
-
-const StyledMain = styled.main`
+const StyledLayoutContainer = styled.div`
   background: ${({ theme }) => theme.color.bgColor};
   color: ${({ theme }) => theme.color.textColor};
+`;
+
+const StyledMain = styled.main`
+  background: inherit;
+  color: inherit;
   width: 1000px;
   margin: 0 auto;
   padding-top: 40px;
